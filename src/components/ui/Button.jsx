1cf6ab4665ee0bedd9c30a4c5cc79e85ebc4dbab
HussamAtharbeh <23110195@htu.edu.jo>
2026-09-10 05:15:28 +0300

    fix: correct CSS import casing

--- src/components/ui/Button.jsx
+++ src/components/ui/Button.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../../styles/componentsStyle/uiStyle/Button.css';
+import '../../styles/componentsStyle/UiStyle/Button.css';
 export const Button = ({ children, type = "button", ...props }) => {
   return (
     <button type={type} className="submit-btn" {...props}>
--- src/components/ui/Input.jsx
+++ src/components/ui/Input.jsx
@@ -1,5 +1,5 @@
 import React from 'react';
-import '../../styles/componentsStyle/uiStyle/Input.css';
+import '../../styles/componentsStyle/UiStyle/Input.css';
 
 export const Input = ({ label, icon: Icon, ...props }) => {
   return (
